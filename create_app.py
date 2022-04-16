d51cae7ceeccb0d495e04671ee03f230dc1ca50a
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-04-16 12:52:40 +0300

    Version 1.2

--- create_app.py
+++ create_app.py
@@ -1,9 +1,6 @@
-import os
-
 from flask import Flask
 from flask_login import LoginManager
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
 
 from data import db_session
 from data.API import user_resources, post_resources, comment_resources
@@ -15,12 +12,12 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
     app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-    file_path = os.path.abspath(os.getcwd()) + "db/NewGramm.db"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NewGramm.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['PROPAGATE_EXCEPTIONS'] = True
     global_init("db/Newgramm.db")
     login_manager = LoginManager()
     login_manager.init_app(app)
-
     api.add_resource(user_resources.UsersListResource, '/api/users')
     api.add_resource(user_resources.UsersResource, '/api/users/<int:user_id>')
     api.add_resource(user_resources.UsersDelete, '/api/users/<int:user_id>/<string:password>')
