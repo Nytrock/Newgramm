dcb6378d233e43d4c52f1811e2d852b0d9eb7564
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-04-16 14:02:40 +0300

    Version 1.4.1.5

--- data/create_post.py
+++ data/create_post.py
@@ -8,6 +8,10 @@ from . import db_session
 from .theme_model import Theme
 
 
+file_path = os.path.join(os.path.abspath('.'), 'db', 'NewGramm.db')
+db_session.global_init(file_path)
+
+
 class PostForm(FlaskForm):
     description = TextAreaField('Описание поста', validators=[DataRequired(), validators.Length(min=4, max=500)])
     image = FileField(u'Фотография', validators=[FileAllowed(['jpg', 'png'])])
